fedd782b648f209de8c8634e648c12e478514285
mo khan <mo@mokhan.ca>
2013-05-20 21:33:17 -0600

    initial commit

---
+++ CalculatorTests/CalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorTests.h
+//  CalculatorTests
+//
+//  Created by mo khan on 2013-05-20.
+//  Copyright (c) 2013 mo khan. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CalculatorTests : SenTestCase
+
+@end
---
+++ CalculatorTests/CalculatorTests.m
@@ -0,0 +1,32 @@
+//
+//  CalculatorTests.m
+//  CalculatorTests
+//
+//  Created by mo khan on 2013-05-20.
+//  Copyright (c) 2013 mo khan. All rights reserved.
+//
+
+#import "CalculatorTests.h"
+
+@implementation CalculatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CalculatorTests");
+}
+
+@end
